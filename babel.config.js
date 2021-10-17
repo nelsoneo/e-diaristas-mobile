7aaf1a5a4d7288f7b93afc4ff17f08e12d560e7d
nelsoneo <nelsoneo@gmail.com>
2021-10-17 10:11:55 -0300

    criando pastas raiz e diretorios

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,20 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          alias: {
+            data: "./src/data",
+            ui: "./src/ui",
+            pages: "./src/pages",
+            "@assets": "./assets",
+            "@styles": "./src/ui/styles",
+          },
+        },
+      ],
+    ],
   };
 };
